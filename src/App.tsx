ae211d9c2bec3c21350fdb06c6ebdbb14562d064
winqus <67459222+winqus@users.noreply.github.com>
2023-01-08 17:01:26 +0200

    Implemented Footer component and added tests.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import './resets.scss';
+import Footer from './components/footer/organisms/Footer/Footer';
+import data from './pageData.json';
+
 function App() {
   return (
-    <div className="App">
-      React & TypeScript
-    </div>
+    <Footer
+      header={data.footer.header}
+      companyTitle={data.footer.companyTitle}
+      copyrightText={data.footer.copyrightText}
+      icons={data.footer.icons}
+    />
   );
 }
 
