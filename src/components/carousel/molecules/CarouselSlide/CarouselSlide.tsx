76b696a56782768c6c9d883771e9114648dab7b1
winqus <67459222+winqus@users.noreply.github.com>
2023-01-13 15:57:31 +0200

    Removed unused property from CarouselSlide.

--- src/components/carousel/molecules/CarouselSlide/CarouselSlide.tsx
+++ src/components/carousel/molecules/CarouselSlide/CarouselSlide.tsx
@@ -5,19 +5,18 @@ interface CarouselSlideProps {
   subtitle: string;
   imageSrc: string;
   altText: string;
-  imageRef?: React.RefObject<HTMLImageElement>;
 }
 
 export default function CarouselSlide({
-  title, subtitle, imageSrc, altText, imageRef,
+  title, subtitle, imageSrc, altText,
 }: CarouselSlideProps) {
   return (
     <li className="carousel__slide">
-      <img ref={imageRef as React.LegacyRef<HTMLImageElement>} className="carousel__image" src={imageSrc} alt={altText} />
+      <img className="carousel__image" src={imageSrc} alt={altText} />
       <div className="carousel__image-overlay-blue" />
       <div className="carousel__text-wrapper">
         <h1 className="carousel__title">{title}</h1>
-        <h1 className="carousel__subtitle">{subtitle}</h1>
+        <h2 className="carousel__subtitle">{subtitle}</h2>
       </div>
     </li>
   );
